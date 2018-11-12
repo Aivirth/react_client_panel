b19b7caa0927d4340981a28d65eb8f11e7ca8e7f
Aivirth <aivirth.work@gmail.com>
2018-11-12 17:43:27 +0100

    Added ClientDetails component

--- src/components/clients/AddClient.js
+++ src/components/clients/AddClient.js
@@ -38,7 +38,7 @@ class AddClient extends Component {
         <div className="row">
           <div className="col-md-6">
             <Link to="/" className="btn btn-link">
-              <i className="fa fa-arrow-cicle-left" /> Back to Dashboard
+              <i className="fas fa-arrow-circle-left" /> Back to Dashboard
             </Link>
           </div>
         </div>
