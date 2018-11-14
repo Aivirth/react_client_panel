78075869f573edc5be8ba95186a8e1cc7fde8b73
Aivirth <aivirth.work@gmail.com>
2018-11-14 15:34:53 +0100

    Added error alert to login form validation

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
-// import { compose } from "redux";
-// import { connect } from "react-redux";
+import { compose } from "redux";
+import { connect } from "react-redux";
 import { firebaseConnect } from "react-redux-firebase";
+
+import { notifyUser } from "../../actions/notifyActions";
+import Alert from "../layout/Alert";
+
 import PropTypes from "prop-types";
 class Login extends Component {
   state = {
@@ -14,7 +18,7 @@ class Login extends Component {
   onSubmit = e => {
     e.preventDefault();
 
-    const { firebase } = this.props;
+    const { firebase, notifyUser } = this.props;
     const { email, password } = this.state;
 
     firebase
@@ -22,15 +26,19 @@ class Login extends Component {
         email: email,
         password: password
       })
-      .catch(err => console.log(err.message));
+      .catch(err => notifyUser(err.message, "error"));
   };
 
   render() {
+    const { message, messageType } = this.props.notify;
     return (
       <div className="row">
         <div className="col-md-6 mx-auto">
           <div className="card">
             <div className="card-body">
+              {message ? (
+                <Alert message={message} messageType={messageType} />
+              ) : null}
               <h1 className="text-center pb-4 pt-3">
                 <span className="text-primary">
                   <i className="fas fa-lock" /> Login
@@ -75,4 +83,14 @@ Login.propTypes = {
   firebase: PropTypes.object.isRequired
 };
 
-export default firebaseConnect()(Login);
+export default compose(
+  firebaseConnect(),
+  connect(
+    (state, props) => ({
+      notify: state.notify
+    }),
+    {
+      notifyUser
+    }
+  )
+)(Login);
